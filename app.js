4eb6822d8f987c754264e7bc593752aed84a7047
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-31 13:12:55 +0800

    git add

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const axios = require('axios')
 
 const app = express()
 const PORT = 3000
@@ -14,6 +15,28 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.render('main')
 })
+
+//try composer api
+const url = 'https://api.openopus.org/composer/list/pop.json'
+
+axios.get(url).then(response => {
+  let data = response.data;
+  let composers = data.composers;
+  for (let i = 0; i < composers.length; i++) {
+    const composer = composers[i];
+    const user = {
+      name: `${composer.name}`,
+      epoch: composer.epoch,
+      portrait: composer.portrait
+    };
+    console.log(user);
+  }
+}).catch(function (error) {
+  console.log(error);
+})
+
+console.log('this is a new page')
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
